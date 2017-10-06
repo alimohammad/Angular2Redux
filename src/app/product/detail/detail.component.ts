86f8a0e6138b36ba387e2fe7a42ac19018a579cc
Mohd Ali <mohd.ali@bertelsmann.de>
2017-10-06 14:32:23 +0800

    detail page working

--- src/app/product/detail/detail.component.ts
+++ src/app/product/detail/detail.component.ts
@@ -1,3 +1,7 @@
+import { IProduct } from './../model/product';
+import { Observable } from 'rxjs/Observable';
+import { IReduxCartState } from './../../reduxcart-store/state';
+import { select } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -6,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './detail.component.html'
 })
 export class DetailComponent implements OnInit {
-
+  @select((s: IReduxCartState) => s.product.uiState.selectedProduct)
+  selectedProduct$: Observable<IProduct>;
   constructor() { }
 
   ngOnInit() {
